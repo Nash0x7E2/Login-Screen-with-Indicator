a1eea448e31cf73251589429dcdeb8d13d9f8adb
Nash Ramdial <ramdialnash@outlook.com>
2018-08-23 19:04:07 -0400

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:login_page_with_indicator_slider/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        accentColor: Colors.amber,
+        primaryColor: Color(0xFFBDA778),
+        iconTheme: IconThemeData(
+          color: Color(0xFFBDA778),
+        ),
+      ),
+      home: SignUpPage(),
+    ),
+  );
+}
